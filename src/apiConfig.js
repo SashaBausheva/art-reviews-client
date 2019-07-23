66e0d3b7a1339a5fabedba96b1ab5a6cbe63d257
SashaBausheva <abausheva@gmail.com>
2019-07-23 12:15:30 -0400

    Fix typo in production URL

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://morning-reaches-38888.herokuapp.com/',
+  production: 'https://morning-reaches-38888.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
