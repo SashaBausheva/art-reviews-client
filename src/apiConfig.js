8856fa8c61682bc635e8ff474d82d95b663bb70e
SashaBausheva <abausheva@gmail.com>
2019-07-23 12:12:17 -0400

    Change production URL

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://morning-reaches-38888.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
