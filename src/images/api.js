1e3b0fe186962d557f6b875fe5dfb55aaa982ee4
SashaBausheva <abausheva@gmail.com>
2019-08-01 00:14:33 -0400

    Remove redundant comments

--- src/images/api.js
+++ src/images/api.js
@@ -58,7 +58,6 @@ export const findImages = (query, user) => {
   return axios({
     params: { query: query },
     url: `${apiUrl}/find-images`,
-    // url: `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${process.env.CLIENT_ID}`,
     method: 'GET',
     headers: {
       'Authorization': `Token token=${user.token}`
@@ -70,7 +69,6 @@ export const findImages = (query, user) => {
 export const findRandomImage = (user) => {
   return axios({
     url: `${apiUrl}/random-image`,
-    // url: `https://api.unsplash.com/photos/random?client_id=${process.env.CLIENT_ID}`,
     method: 'GET',
     headers: {
       'Authorization': `Token token=${user.token}`
@@ -81,7 +79,6 @@ export const findRandomImage = (user) => {
 export const findRandomImages = () => {
   return axios({
     url: `${apiUrl}/random-images`,
-    // url: 'https://api.unsplash.com/photos/random?count=5&client_id=27e3739785506041845aa8e7be75f77231b1baaeb1ddadc128233d959a39e201',
     method: 'GET'
   })
 }
